394386cc13d7b92d42cf5db98af2d23c971195f0
HINDA <monir.hinda@gmail.com>
2026-06-07 11:08:53 +0200

    fetch finished

--- pipeline/fetch.py
+++ pipeline/fetch.py
@@ -1,20 +1,43 @@
 import requests 
 import csv
+import time
+
 url = "https://steamspy.com/api.php"
+all_games=[]
+
+for page in range (0,3):
+    parametres={
+        "request": "all",
+        "page": page}
+
+    try:
+        response = requests.get(url, params=parametres)
+        response.raise_for_status()  #declenche une exception pour les codes d'erreur HTTP
+        data = response.json()  #convertit json en dictionnaire 
+    except requests.RequestException as e:
+        print(f"Error fetching data: {e}")
+        break
 
 
-parametres={
-    "request": "all",
-    "page": 0}
+    #print(response.status_code==200)
+    #print(response.text[:500])
 
-response = requests.get(url, params=parametres)
-print(response.status_code==200)
+    #print(type(données))
+    #print(len(données))
 
-##print(response.text[:500])
+    games = list(data.values())
+    all_games.extend(games)
+    print(f"  → {len(games)} jeux récupérés (total : {len(all_games)})")
+    
+    print("Pause 60 secondes...")
+    time.sleep(60)
 
-données = response.json()  #convertit json en dictionnaire 
-print(type(données))
-print(len(données))
+   
+file = open("data/raw/steamspy_3pages.csv", "w", newline="", encoding="utf-8") 
 
-premier_jeu = list(données.values())[0]
-print(premier_jeu)
+writer = csv.DictWriter(file, fieldnames=games[0].keys())
+writer.writeheader()
+writer.writerows(all_games)
+file.close();
+
+print(f"{len(all_games)} jeux sauvegardés")
