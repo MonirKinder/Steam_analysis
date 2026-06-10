bd0973c907e1ba5cf280d45c65b6c14e7f2fe8a9
HINDA <monir.hinda@gmail.com>
2026-06-10 20:30:35 +0200

    finished clean that creates a new file

--- pipeline/clean.py
+++ pipeline/clean.py
@@ -1,8 +1,16 @@
 import pandas as pd
 from pathlib import Path
+from datetime import datetime
+
 
 raw_dir = Path("data/raw")
-fichiers = sorted(raw_dir.glob("steamspy_*.csv"))
+clean_dir = Path("data/clean")
+
+clean_dir.mkdir(parents=True, exist_ok=True)         #crée le dossier clean s'il n'existe pas déjà
+timestamp = datetime.now().strftime("%Y%m%d_%H%M")  #format de timestamp pour le nom du fichier de sortie
+output_file = clean_dir / f"steamspy_clean_{timestamp}.csv"  #chemin du fichier de sortie
+
+fichiers = sorted(raw_dir.glob("steamspy_*.csv"))    #trie les fichiers par ordre alphabétique, pour recupérer le dernier fichier
 dernier_fichier = fichiers[-1]
 
 
@@ -13,6 +21,7 @@ print(df.isnull().sum())
 
 
 #clean owners---------------------------------------------
+print('owners')
 print(df["owners"].head(10))
 
 df["owners_lower"] = (
@@ -27,11 +36,12 @@ df["owners_lower"] = (
 print(df["owners_lower"].head(10))
 
 #clean price---------------------------------------------
-print('price')
-print(df["price"].head(10))
-print(df["price"].dtype)
+#print('price')
+#print(df["price"].head(10))
+#print(df["price"].dtype)
 
 df["price_eur"] = (df["price"] / 100).round(2)
+print('price_eur')
 print(df["price_eur"].head(10))
 
 #clean developer et publisher score rank---------------------------------------------
@@ -48,11 +58,42 @@ df["review_ratio"] = (
     .round(4)
     )
 
-print(df["review_ratio"])
+print('review_ratio')
 print(df["review_ratio"].head(10))
 
 
 #clean dulpluicates no name---------------------------------------------
 df = df.drop_duplicates(subset="appid", keep="first")
 df = df[df["name"].notna()]
-df = df[~((df["owners_lower"] == 0) & (df["positive"] == 0))]
+df = df[(df["owners_lower"] != 0) | (df["positive"] != 0)]
+
+
+
+
+
+colonnes_finales = [
+    "appid",
+    "name",
+    "developer",
+    "publisher",
+    "owners_lower",
+    "price_eur",
+    "discount",
+    "positive",
+    "negative",
+    "review_ratio",
+    "score_rank",
+    "average_forever",
+    "average_2weeks",
+    "median_forever",
+    "ccu"
+]
+
+df = df[colonnes_finales]
+print(df.shape)
+print(df.head())
+
+
+
+df.to_csv(output_file, index=False, encoding="utf-8")
+print(f"Fichier propre sauvegardé : {output_file}")
